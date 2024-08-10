ebb9eeb8be623d2fd80f042f5c9eda591eaabc91
takezawamiki <mikiaaamiki@yahoo.co.jp>
2024-08-10 14:44:41 +0900

    投稿機能を実装しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,23 @@ class PostsController < ApplicationController
   def new
       @post = Post.new
   end
+  
+  def create
+      @post = Post.new(post_params)
+      if @post.save
+          redirect_to 'posts_path,notice: 'つぶやきを投稿しました'
+      else
+          renderf :new
+      end
+  end
 
   def edit
+  end
+  
+  
+  private
+  
+  def post_params
+      params.require(:post).permit(:content)
   end
 end
